6f335239cafe046a96d9893b8707d5e7a18f1e7c
Quang Duy <phamzuy2003@gmail.com>
2024-03-06 02:06:02 +0700

    init productDAO and CategoryDAO

---
+++ src/java/dal/CategoryDAO.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+
+import dal.GenericDAO;
+import java.util.List;
+import model.Category;
+
+/**
+ *
+ * @author Admin
+ */
+public class CategoryDAO extends GenericDAO<Category>{
+
+    @Override
+    public List<Category> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int insert(Category t) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
